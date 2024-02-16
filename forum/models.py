bc5060c41eeeef8aec87ac23848ff996da09b1b7
oiltema <art.lobanovskiy@gmail.com>
2024-02-16 16:46:17 +0300

    first migration

--- forum/models.py
+++ forum/models.py
@@ -1,3 +1,10 @@
 from django.db import models
 
 # Create your models here.
+
+class Post(models.Model):
+    title = models.CharField(max_length=255, verbose_name='Заголовок')
+    content = models.TextField(max_length=1500, verbose_name='Статья')
+    rating = models.IntegerField()
+    author = models.CharField(max_length=255, verbose_name='Автор')
+
